e49a426ac58406ab2e1457fa528c6d81b3f8fee2
veresrobert <veresrobert20@gmail.com>
2023-07-07 15:48:15 +0300

    JS Tema 02

---
+++ app.js
@@ -0,0 +1,91 @@
+// Ex. 1
+// for (let n = 0; n <= 10; n++) {
+//     if (n % 2 === 0 ){
+//         console.log(n)
+//     }
+// }
+
+// Ex. 2 ----------------------------------
+// function TheSumOfTheNumbers(array) {
+//       let sum = 0;
+//       for (let i = 0; i < array.length; i++) {
+//         sum += array[i]; 
+//       }
+//       return sum;
+//     }
+    
+    
+//     const numbers = [53, 567, -345, 65, -200, 345, -334];
+//     console.log(TheSumOfTheNumbers(numbers)); 
+
+ // Ex. 3 --------------------------------- 
+    
+//  let numbers = [34, 3, 15, 44, 53, 123, 75,];
+//  console.log("numbers",numbers);
+
+//  let reversed = numbers.reverse()
+//  console.log("reversed",reversed)
+
+// Ex. 4------------------------------------
+// function vowelCount(str) {
+//     var count = 0
+//     var vowels = ["a", "e", "i", "o", "u"]
+
+//     for (let letter of str.toLowerCase()) {
+//         if (vowels.includes(letter)) {
+//         count++;
+//         }
+//     }
+//     return count
+    
+// }
+
+// let string = prompt("Enter a string: ");
+
+// let result = vowelCount(string);
+
+// console.log("This string has " + result + " vowels in it");
+
+/* sa fiu sincer cu tine la  exercitiul 4 
+am cautat pe google cum sa fac  */
+
+
+// Ex. Extra de la Raducu Roman  ------------
+
+// let nume = "veres";
+// let prenume = "roberth";
+
+
+
+// nume = nume.split("")
+// prenume = prenume.split("")
+
+
+// let firstLetter = nume[0]
+// let firstLetterLastName = prenume[0]
+
+// nume[0] = firstLetter.toUpperCase()
+// prenume[0] = firstLetterLastName.toUpperCase()
+
+
+// let Nume = "";
+
+// for (let i = 0; i < nume.length; i++) {
+//     Nume = Nume + nume [i]
+// }
+
+// let Prenume = "";
+
+// for (let i = 0; i < prenume.length; i++) {
+//     Prenume = Prenume + prenume[i]
+// }
+
+// const MyName = Nume + " " + Prenume
+ 
+// console.log("My name is: ", MyName )
+
+
+
+
+
+
